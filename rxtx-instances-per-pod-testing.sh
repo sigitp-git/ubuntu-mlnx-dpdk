6960466b9478d9f72e1af2e6a59a976829da087b
sigitp <sigit.priyanggoro@gmail.com>
2024-09-04 08:37:01 -0500

    Update rxtx-instances-per-pod-testing.sh

--- rxtx-instances-per-pod-testing.sh
+++ rxtx-instances-per-pod-testing.sh
@@ -1,1 +1,5 @@
+## Pod1
+./build/app/dpdk-testpmd -l 1,2,97,98 -n 4 -a 02:00.5 --file-prefix sigitp-dpdk-test --socket-mem=1024,0 --proc-type=auto -- --nb-cores=3 --rxq=4 --txq=4 -i --forward-mode=txonly --eth-peer=0,7e:2b:33:59:0b:2e
+./build/app/dpdk-testpmd -l 1,2,97,98 -n 4 -a 02:00.5 --file-prefix sigitp-dpdk-test --socket-mem=1024,0 --proc-type=auto -- --nb-cores=3 --rxq=4 --txq=4 -i --forward-mode=rxonly --eth-peer=0,7e:2b:33:59:0b:2e
 
+## Pod2
