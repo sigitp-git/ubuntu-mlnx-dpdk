c41368cefa11e571e361ee264d6f4f8f6c881fe0
sigitp <sigit.priyanggoro@gmail.com>
2024-09-03 22:02:34 -0500

    Update exec-testing.sh

--- exec-testing.sh
+++ exec-testing.sh
@@ -132,6 +132,7 @@ testpmd>
 ####################################################################################################################################################################################
 ## Flowgen retry on both end
 ####################################################################################################################################################################################
+## Pod1
 testpmd> set fwd flowgen retry
 Set flowgen packet forwarding mode with retry
 testpmd> start
@@ -159,3 +160,110 @@ Logical Core 98 (socket 0) forwards packets on 2 streams:
       TX desc=2048 - TX free threshold=0
       TX threshold registers: pthresh=0 hthresh=0  wthresh=0
       TX offloads=0x10000 - TX RS bit threshold=0
+
+testpmd> show port stats 0
+
+  ######################## NIC statistics for port 0  ########################
+  RX-packets: 0          RX-missed: 0          RX-bytes:  0
+  RX-errors: 0
+  RX-nombuf:  0         
+  TX-packets: 64400892352 TX-errors: 0          TX-bytes:  3971245344640
+
+  Throughput (since last show)
+  Rx-pps:            0          Rx-bps:            0
+  Tx-pps:    107614610          Tx-bps:  51655013168
+  ############################################################################
+testpmd> 
+
+testpmd> show fwd stats all
+
+  ------- Forward Stats for RX Port= 0/Queue= 0 -> TX Port= 0/Queue= 0 -------
+  RX-packets: 0              TX-packets: 13079777024    TX-dropped: 0             
+
+  ------- Forward Stats for RX Port= 0/Queue= 1 -> TX Port= 0/Queue= 1 -------
+  RX-packets: 0              TX-packets: 19690080832    TX-dropped: 0             
+
+  ------- Forward Stats for RX Port= 0/Queue= 2 -> TX Port= 0/Queue= 2 -------
+  RX-packets: 0              TX-packets: 6437547488     TX-dropped: 0             
+
+  ------- Forward Stats for RX Port= 0/Queue= 3 -> TX Port= 0/Queue= 3 -------
+  RX-packets: 0              TX-packets: 6437547552     TX-dropped: 0             
+
+  ---------------------- Forward statistics for port 0  ----------------------
+  RX-packets: 0              RX-dropped: 0             RX-total: 0
+  TX-packets: 45644954144    TX-dropped: 0             TX-total: 45644954144
+  ----------------------------------------------------------------------------
+
+  +++++++++++++++ Accumulated forward statistics for all ports+++++++++++++++
+  RX-packets: 0              RX-dropped: 0             RX-total: 0
+  TX-packets: 45644954144    TX-dropped: 0             TX-total: 45644954144
+  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+testpmd> 
+
+## Pod2
+testpmd> set fwd flowgen retry
+Set flowgen packet forwarding mode with retry
+testpmd> start
+flowgen packet forwarding with retry - ports=1 - cores=3 - streams=4 - NUMA support enabled, MP allocation mode: native
+TX retry num: 64, delay between TX retries: 1us
+Logical Core 3 (socket 0) forwards packets on 1 streams:
+  RX P=0/Q=0 (socket 0) -> TX P=0/Q=0 (socket 0) peer=AA:1E:C6:17:9B:FC
+Logical Core 98 (socket 0) forwards packets on 1 streams:
+  RX P=0/Q=1 (socket 0) -> TX P=0/Q=1 (socket 0) peer=AA:1E:C6:17:9B:FC
+Logical Core 99 (socket 0) forwards packets on 2 streams:
+  RX P=0/Q=2 (socket 0) -> TX P=0/Q=2 (socket 0) peer=AA:1E:C6:17:9B:FC
+  RX P=0/Q=3 (socket 0) -> TX P=0/Q=3 (socket 0) peer=AA:1E:C6:17:9B:FC
+
+  number of flows for port 0: 1024
+  flowgen packet forwarding with retry packets/burst=32
+  packet len=64 - nb packet segments=1
+  nb forwarding cores=3 - nb forwarding ports=1
+  port 0: RX queue number: 4 Tx queue number: 4
+    Rx offloads=0x0 Tx offloads=0x10000
+    RX queue: 0
+      RX desc=2048 - RX free threshold=64
+      RX threshold registers: pthresh=0 hthresh=0  wthresh=0
+      RX Offloads=0x0
+    TX queue: 0
+      TX desc=2048 - TX free threshold=0
+      TX threshold registers: pthresh=0 hthresh=0  wthresh=0
+      TX offloads=0x10000 - TX RS bit threshold=0
+
+testpmd> show port stats 0
+
+  ######################## NIC statistics for port 0  ########################
+  RX-packets: 6340968450 RX-missed: 1245923873 RX-bytes:  405821980800
+  RX-errors: 0
+  RX-nombuf:  0         
+  TX-packets: 46746679104 TX-errors: 0          TX-bytes:  2804800746240
+
+  Throughput (since last show)
+  Rx-pps:            0          Rx-bps:            0
+  Tx-pps:    108284211          Tx-bps:  51976421504
+  ############################################################################
+testpmd> 
+
+testpmd> show fwd stats all
+
+  ------- Forward Stats for RX Port= 0/Queue= 0 -> TX Port= 0/Queue= 0 -------
+  RX-packets: 0              TX-packets: 14456629056    TX-dropped: 0             
+
+  ------- Forward Stats for RX Port= 0/Queue= 1 -> TX Port= 0/Queue= 1 -------
+  RX-packets: 0              TX-packets: 22758399872    TX-dropped: 0             
+
+  ------- Forward Stats for RX Port= 0/Queue= 2 -> TX Port= 0/Queue= 2 -------
+  RX-packets: 0              TX-packets: 7092221792     TX-dropped: 0             
+
+  ------- Forward Stats for RX Port= 0/Queue= 3 -> TX Port= 0/Queue= 3 -------
+  RX-packets: 0              TX-packets: 7092221792     TX-dropped: 0             
+
+  ---------------------- Forward statistics for port 0  ----------------------
+  RX-packets: 0              RX-dropped: 0             RX-total: 0
+  TX-packets: 51399473216    TX-dropped: 0             TX-total: 51399473216
+  ----------------------------------------------------------------------------
+
+  +++++++++++++++ Accumulated forward statistics for all ports+++++++++++++++
+  RX-packets: 0              RX-dropped: 0             RX-total: 0
+  TX-packets: 51399473216    TX-dropped: 0             TX-total: 51399473216
+  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+testpmd> 
