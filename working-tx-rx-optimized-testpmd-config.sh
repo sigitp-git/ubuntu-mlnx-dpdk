2e31e37e31f49b024b78840e55329193921c4626
sigitp-git <sigit.priyanggoro@gmail.com>
2024-09-18 00:13:24 +0000

    kubectl cpu limit pods check

--- working-tx-rx-optimized-testpmd-config.sh
+++ working-tx-rx-optimized-testpmd-config.sh
@@ -31,3 +31,49 @@ testpmd> show port stats 0
   Tx-pps:            0          Tx-bps:            0
   ############################################################################
 testpmd> 
+
+## Checking CPU allocation on the pods
+ubuntu@ip-172-31-26-158:~$ kubectl exec -it mlnx-dpdk-dsf-node1 -- sh -c 'KUBEPOD_SLICE=$(cut -d: -f3 /proc/self/cgroup); cat /sys/fs/cgroup$KUBEPOD_SLICE/cpuset.cpus.effective'
+49-64,144-160
+
+ubuntu@ip-172-31-26-158:~$ kubectl exec -it mlnx-dpdk-dsf-node2 -- sh -c 'KUBEPOD_SLICE=$(cut -d: -f3 /proc/self/cgroup); cat /sys/fs/cgroup$KUBEPOD_SLICE/cpuset.cpus.effective'
+49-64,144-160
+
+## Checking CPU allocation on the hosts
+node-1$ sudo cat /var/lib/kubelet/cpu_manager_state | jq
+{
+  "policyName": "static",
+  "defaultCpuSet": "0-47,65-95,97-143,161-191",
+  "entries": {
+    "a36ce0ef-3989-4bd8-993d-0fba17c44b89": {
+      "ubuntu-netutils": "48"
+    },
+    "af9c544c-dbc1-40f6-9392-dc5187b8c04c": {
+      "ubuntu-mlnx-dpdk": "49-64,144-160"
+    },
+    "ed292075-3565-4919-83dc-248e3bf09ead": {
+      "ubuntu-frr": "96"
+    }
+  },
+  "checksum": 470772457
+}
+node-1$ 
+
+node-2$ sudo cat /var/lib/kubelet/cpu_manager_state | jq
+{
+  "policyName": "static",
+  "defaultCpuSet": "0-47,65-95,97-143,161-191",
+  "entries": {
+    "2ca9fce0-898b-4f5b-bd4e-4d4faf20935c": {
+      "ubuntu-frr": "96"
+    },
+    "bced91a0-2e23-4a5e-98b5-1887239e7c9a": {
+      "ubuntu-mlnx-dpdk": "49-64,144-160"
+    },
+    "d8f48b9f-fad8-40d2-a222-2286a88f6129": {
+      "ubuntu-netutils": "48"
+    }
+  },
+  "checksum": 67353168
+}
+node-2$ 
